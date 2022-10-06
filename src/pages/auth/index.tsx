390930bf5959eaf492d6581ca0c565d2b149c6a2
Luke Fisk-Lennon <luke.fisklennon@gmail.com>
2022-10-06 19:00:21 +1100

    Auth directory

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import PageTemplate from "../../templates/PageTemplate.tsx";
+
+const User = () => {
+
+}
+
+const AuthPage = () => {
+  return (
+    <PageTemplate>
+      <div>Auth content</div>
+    </PageTemplate>
+  )
+}
+
+export default AuthPage;
