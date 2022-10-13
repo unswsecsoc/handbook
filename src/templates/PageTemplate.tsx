aef9f2caf10ce2361cd4fc060723daa06faeafe2
Jonah Hopkin <jonahhopkin@gmail.com>
2022-10-13 15:47:54 +1100

    First steps in sidebar development

--- src/templates/PageTemplate.tsx
+++ src/templates/PageTemplate.tsx
@@ -9,9 +9,7 @@ interface PageTemplateProps {
 const PageTemplate = ({ children }: PageTemplateProps) => {
   return (
     <div className={style.page}>
-      <div className={style.sidebar}>
-        <Sidebar />
-      </div>
+      <Sidebar />
       <main className={style.content}>
         {children}
       </main>
