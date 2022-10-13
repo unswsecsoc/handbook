d330ff1531d04e9b9bf022eb5ad0ad1c44d27f52
tlwns <87194559+tlwns@users.noreply.github.com>
2022-10-13 18:15:30 +1100

    Added users cards feature

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,14 +1,56 @@
 import * as React from "react";
 import PageTemplate from "../../templates/PageTemplate.tsx";
+import * as style from "./auth.module.css"
 
-const User = () => {
+type User = { authToken: string, accessLevel: string, name: string };
+const users: User[] = [
+  { authToken: '1234a', accessLevel: '1', name: 'Joe' },
+  { authToken: '1234b', accessLevel: '2', name: 'Bob' },
+  { authToken: '1234c', accessLevel: '3', name: 'Harold' },
+  { authToken: '1234d', accessLevel: '4', name: 'Nick' }
+]
 
+type UserProps = { user: User }
+
+const Users = ({ user }: UserProps) => {
+  return (
+    <div className={style.User} id={style[`User${user.name}`]}>
+      <div className={style.UserInfo}>
+        <b>{user.name}</b>
+        <p>
+          Authentication token<br></br>
+          {user.authToken}<br></br>
+        </p>
+        <p>
+          Authorisation level<br></br>
+          {user.accessLevel}<br></br>
+        </p>
+      </div>
+    </div >
+  );
+}
+
+const Cards = () => {
+  return (
+    <div className={style.BlockContainer}>
+      <div className={style.Users}>
+        {
+          users.map((user, index) => {
+            return (
+              <Users user={user} key={index} />
+            );
+          })
+        }
+      </div>
+
+    </div >
+  );
 }
 
 const AuthPage = () => {
   return (
     <PageTemplate>
-      <div>Auth content</div>
+      <Cards />
     </PageTemplate>
   )
 }
