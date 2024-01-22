9e304a949e7c5788b5c70a161f1796f694d6d64a
Westley <lowestley0409@gmail.com>
2024-01-22 18:40:29 +1100

    made a carousel thingy

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -20,10 +20,18 @@ interface SearchInit {
 function Search({ tag }: SearchInit) {
   const [query, setquery] = useState("");
   const fuse = new Fuse(allPosts, options);
-  const posts = query.length > 0 ? fuse.search(query).map((post) => post.item) : allPosts
+  const posts =
+    query.length > 0
+      ? fuse
+          .search(query)
+          .map((post) => post.item)
+          .sort((a, b) => Number(a.data.pubDate < b.data.pubDate))
+      : allPosts;
 
   const [open, setOpen] = useState(false);
-  const [selectedTags, setselectedTags] = useState<string[]>([tag]);
+  const [selectedTags, setselectedTags] = useState<string[]>(
+    tag.length > 0 ? [tag] : []
+  );
 
   const handlesearch = (event: ChangeEvent<HTMLInputElement>) => {
     setquery(event.currentTarget.value);
@@ -52,16 +60,13 @@ function Search({ tag }: SearchInit) {
       </div>
 
       {/* tag selector here*/}
-
       <div
-        className=" my-2 relative flex items-center gap-2 w-full h-6 p-2 text-sm leading-loose border rounded-lg outline-none focus:border-green-500"
-        // onBlur={() => setOpen(false)}
+        className=" my-2 relative flex items-center gap-2 w-full px-2 text-sm leading-loose border rounded-lg outline-none focus:border-green-500"
         onClick={() => setOpen((prev) => !prev)}
         tabIndex={0}
       >
         <span className=" flex-grow flex gap-2 flex-wrap">
           {selectedTags.map((tag) => (
-
             <button
               key={tag}
               onClick={(e) => {
@@ -88,20 +93,26 @@ function Search({ tag }: SearchInit) {
         <div className=" translate-x-0 translate-y-1/4 border-[.25em] border-solid border-transparent border-t-[#777]"></div>
       </div>
 
-      <ul className={` m-0 p-0 list-none ${open ? "" : "hidden" } max-h-60 overflow-y-auto border-[.25em] border-solid border-[#777] rounded w-full left-0 top-[calc(100%+0.25em)] z-50`}>
-        {uniqueTags.filter((tag) => !selectedTags.includes(tag)).map((option) => (
-          <li
-            onClick={(e) => {
-              e.stopPropagation()
-              console.log(option)
-              selectTag(option)
-            }}
-            key={option}
-            className=" px-2 py-1 cursor-pointer hover:bg-emerald-700 transition-all"
-          >
-            {option}
-          </li>
-        ))}
+      <ul
+        className={` m-0 p-0 list-none ${
+          open ? "" : "hidden"
+        } max-h-60 overflow-y-auto border-[.25em] border-solid border-[#777] rounded w-full left-0 top-[calc(100%+0.25em)] z-50`}
+      >
+        {uniqueTags
+          .filter((tag) => !selectedTags.includes(tag))
+          .map((option) => (
+            <li
+              onClick={(e) => {
+                e.stopPropagation();
+                console.log(option);
+                selectTag(option);
+              }}
+              key={option}
+              className=" px-2 py-1 cursor-pointer hover:bg-emerald-700 transition-all"
+            >
+              {option}
+            </li>
+          ))}
       </ul>
 
       <p className=" w-full justify-start py-3 text-lg">
@@ -111,14 +122,18 @@ function Search({ tag }: SearchInit) {
 
       <ul className="w-full">
         {posts &&
-          posts.filter(post => selectedTags.every(e => post.data.tags.includes(e))).map((post) => (
-            <a href={`/posts/${post.slug}/`}>
-              <li className=" bg-slate-600 py-3 px-2 my-1 rounded-lg bg-gradient-to-tl from-red-800 to-teal-600 hover:scale-[1.04] transition-all cursor-pointer">
-                <h1 className="text-xl font-extrabold">{post.data.title}</h1>
-                <p>{post.data.description.slice(0, 100)}</p>
-              </li>
-            </a>
-          ))}
+          posts
+            .filter((post) =>
+              selectedTags.every((e) => post.data.tags.includes(e))
+            )
+            .map((post) => (
+              <a href={`/posts/${post.slug}/`}>
+                <li className=" bg-slate-600 py-3 px-2 my-1 rounded-lg bg-gradient-to-tl from-red-800 to-teal-600 hover:scale-[1.04] transition-all cursor-pointer">
+                  <h1 className="text-xl font-extrabold">{post.data.title}</h1>
+                  <p>{post.data.description.slice(0, 100)}</p>
+                </li>
+              </a>
+            ))}
       </ul>
     </>
   );
